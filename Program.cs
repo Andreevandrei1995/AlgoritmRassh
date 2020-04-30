604a3773b532d666ca0c583121d67ed3870592de
Andrey <dffdf>
2020-04-30 20:12:24 +0300

    master. add reflection example

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,18 @@ namespace AlgoritmRassh
     {
         static void Main(string[] args)
         {
+            Reflection reflection = new Reflection();
+            reflection.a = "5";
+            reflection.b = "6";
+            Type type = typeof(Reflection);
+            FieldInfo fieldInfo = type.GetField("a");
+            Console.WriteLine(fieldInfo.GetValue(reflection));
         }
+    }
+
+    class Reflection
+    {
+        public string a;
+        public string b;
     }
 }
