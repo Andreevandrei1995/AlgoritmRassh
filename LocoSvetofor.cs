1ad142538a0493d10914124cc8761fd4203d147a
MVTShishkanova <MVTShishkanova@MVTI7-22>
2020-05-01 17:37:51 +0300

    master.locoSvet

---
+++ LocoSvetofor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmRassh
+{
+    class LocoSvetofor
+    {
+        /// <summary>
+        /// Цвет светофора
+        /// </summary>
+        private string Light;
+
+        public LocoSvetofor
+
+        public string GetLight()
+        {
+            return Light;
+        }
+
+    }
+}
