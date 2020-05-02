680cd117209d1ae219ba33c2c8ff162c17f7694a
Andrey <dffdf>
2020-05-02 22:50:04 +0300

    master. Изменение структуры прокта.

---
+++ BaseClassesAndInterfaces/Velocity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmRassh
+{
+    class Velocity
+    {
+        public double value;
+        public Velocity(double value)
+        {            
+            this.value = value;
+        }
+    }
+}
